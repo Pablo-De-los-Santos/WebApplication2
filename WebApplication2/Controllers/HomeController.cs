1f7f0599e8899c304e28da938cde6ba7918633e4
Niels De Los Santos <kilohdnartis@gmail.com>
2022-07-23 01:26:40 -0400

    Consumir datos de un servicio

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -9,9 +9,11 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
 using WebApplication2.DBContext;
 using WebApplication2.Models;
+using WebApplication2.Dto;
 
 namespace WebApplication2.Controllers
 {
@@ -32,25 +34,26 @@ namespace WebApplication2.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Index(IFormFile files)
+        public IActionResult Index(IFormFile files, string url = "")
         {
 
             var rootPath = Environment.WebRootPath;
-
-            var fullPath = Path.Combine(rootPath, files.FileName);
-            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
-            {
-                files.CopyTo(stream);
-            }
-
             var Datos = new Dto.File()
             {
                 encabezado = new Dto.Encabezado { },
                 detalles = new List<Dto.Detalle>() { }
             };
-
             if (files != null)
             {
+                var fullPath = Path.Combine(rootPath, files.FileName);
+                using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    files.CopyTo(stream);
+                }
+
+
+
+
                 string path = Path.Combine(Environment.WebRootPath, "Nom1");
                 if (!Directory.Exists(path))
                 {
@@ -121,23 +124,171 @@ namespace WebApplication2.Controllers
                                 Discapacidad = fila[10]
                             });
 
-                            int id = _context.Students.OrderByDescending(t=>t.StudentId).FirstOrDefault().StudentId;
+                            int id = _context.Students.OrderByDescending(t => t.StudentId).FirstOrDefault().StudentId;
 
-                            foreach (var nota in Notas) 
+                            foreach (var nota in Notas)
                             {
                                 SaveRating(new Dto.Calificaciones { Course = nota.Course, Score = nota.Score }, id);
                             }
 
                         }
                 }
-                
+
                 return View(Datos);
+            }
+            else if (url != "")
+            {
+
+                var csvData = Get(url);
+                bool PrimeraLina = true;
+                foreach (var File in csvData)
+                {
+                    if (File != null)
+                        if (PrimeraLina)
+                        {
+
+                            CultureInfo provider = CultureInfo.InvariantCulture;
+
+                            Datos.encabezado.Fecha = DateTime.ParseExact(File.FechaRemision, "ddMMyyyy", provider);
+                            Datos.encabezado.CodigoCebtro = File.CodigoCentro;
+                            Datos.encabezado.CodigoPlantel = File.CodigoPlantel;
+                            PrimeraLina = false;
+
+                        }
+                        else
+                        {
+                            var Notas = new List<Dto.Calificaciones>();
+
+
+                            foreach (var Calificaciones in File.InformacionAcademica.Split(','))
+                            {
+                                string[] Calificaion = Calificaciones.Split(':');
+                                Notas.Add(new Dto.Calificaciones
+                                {
+                                    Course = Calificaion[0],
+                                    Score = int.Parse(Calificaion[1])
+                                });
+                            }
+
+                            Datos.detalles.Add(new Dto.Detalle
+                            {
+                                NumeroDeDocumento = File.CodigoCentroMinerd,
+                                Rne = File.Rne,
+                                Municipio = File.MunicipioResidencia,
+                                Periodo = File.PeriodoAcademico,
+                                Nivel = File.NivelAcademico,
+                                Grado = int.Parse(File.GradoAcademico),
+                                Calificaiones = Notas,
+                                Tipodesangre = File.TipodeSangre,
+                                Discapacidad = File.CondicionesDiscapacidad
+                            });
+
+
+                            SaveStudent(new Dto.Detalle
+                            {
+                                NumeroDeDocumento = File.CodigoCentroMinerd,
+                                Rne = File.Rne,
+                                Municipio = File.MunicipioResidencia,
+                                Periodo = File.PeriodoAcademico,
+                                Nivel = File.NivelAcademico,
+                                Grado = int.Parse(File.GradoAcademico),
+                                Calificaiones = Notas,
+                                Tipodesangre = File.TipodeSangre,
+                                Discapacidad = File.CondicionesDiscapacidad
+                            });
+
+                            int id = _context.Students.OrderByDescending(t => t.StudentId).FirstOrDefault().StudentId;
+
+                            foreach (var nota in Notas)
+                            {
+                                SaveRating(new Dto.Calificaciones { Course = nota.Course, Score = nota.Score }, id);
+                            }
+
+                        }
+                }
+
+                return View(Datos);
+            }
+            else if (files == null && url == "")
+            {
+
+                string csvData = Ftprecive();
+                bool PrimeraLina = true;
+                foreach (var File in csvData.Split('\n'))
+                {
+                    if (File != "")
+                        if (PrimeraLina)
+                        {
+
+                            CultureInfo provider = CultureInfo.InvariantCulture;
+                            string[] fila = File.Split('|');
+                            Datos.encabezado.Fecha = DateTime.ParseExact(fila[2], "ddMMyyyy", provider);
+                            Datos.encabezado.CodigoCebtro = fila[3];
+                            Datos.encabezado.CodigoPlantel = fila[4];
+                            PrimeraLina = false;
+
+                        }
+                        else
+                        {
+                            var Notas = new List<Dto.Calificaciones>();
+                            string[] fila = File.Split('|');
+
+                            foreach (var Calificaciones in fila[8].Split(','))
+                            {
+                                string[] Calificaion = Calificaciones.Split(':');
+                                Notas.Add(new Dto.Calificaciones
+                                {
+                                    Course = Calificaion[0],
+                                    Score = int.Parse(Calificaion[1])
+                                });
+                            }
+
+                            Datos.detalles.Add(new Dto.Detalle
+                            {
+                                NumeroDeDocumento = fila[2],
+                                Rne = fila[3],
+                                Municipio = fila[4],
+                                Periodo = fila[5],
+                                Nivel = fila[6],
+                                Grado = int.Parse(fila[7]),
+                                Calificaiones = Notas,
+                                Tipodesangre = fila[9],
+                                Discapacidad = fila[10]
+                            });
+
+
+                            SaveStudent(new Dto.Detalle
+                            {
+                                NumeroDeDocumento = fila[2],
+                                Rne = fila[3],
+                                Municipio = fila[4],
+                                Periodo = fila[5],
+                                Nivel = fila[6],
+                                Grado = int.Parse(fila[7]),
+                                Calificaiones = Notas,
+                                Tipodesangre = fila[9],
+                                Discapacidad = fila[10]
+                            });
+
+                            int id = _context.Students.OrderByDescending(t => t.StudentId).FirstOrDefault().StudentId;
+
+                            foreach (var nota in Notas)
+                            {
+                                SaveRating(new Dto.Calificaciones { Course = nota.Course, Score = nota.Score }, id);
+                            }
+
+                        }
+                }
+
+                return View(Datos);
+
             }
             return View();
         }
-        public void SaveStudent(Dto.Detalle Studiante) 
+        public void SaveStudent(Dto.Detalle Studiante)
         {
-            _context.Students.Add(new Students {
+            _context.Students.Add(new Students
+            {
                 AcademicLevel = Studiante.Nivel,
                 BloodType = Studiante.Tipodesangre,
                 ConditionDescription = Studiante.Discapacidad,
@@ -149,23 +300,113 @@ namespace WebApplication2.Controllers
 
             });
             _context.SaveChanges();
-           
+
 
         }
 
-        public void SaveRating(Dto.Calificaciones Rating,int id)
+        public void SaveRating(Dto.Calificaciones Rating, int id)
         {
             _context.Ratings.Add(new Ratings
             {
-               StudentId = id,
-               Course = Rating.Course,
-               Score = Rating.Score
+                StudentId = id,
+                Course = Rating.Course,
+                Score = Rating.Score
 
             });
             _context.SaveChanges();
 
 
         }
+        private string Ftprecive()
+        {
+            const string site = "ftp://files.000webhost.com//";
+            string NombreArchivo = $"LICEO ROSS.txt";
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(site + @"\" + NombreArchivo);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.KeepAlive = true;
+            request.UsePassive = false;
+            request.UseBinary = false; // use true for .zip file or false for a text file
+
+            request.Credentials = new NetworkCredential("niels1709", "niels1709");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+
+            using (FileStream writer = new FileStream(NombreArchivo, FileMode.Create))
+            {
+                long length = response.ContentLength;
+                int bufferSize = 2048;
+                int readCount;
+                byte[] buffer = new byte[2048];
+
+                readCount = responseStream.Read(buffer, 0, bufferSize);
+                while (readCount > 0)
+                {
+                    writer.Write(buffer, 0, readCount);
+                    readCount = responseStream.Read(buffer, 0, bufferSize);
+                }
+            }
+
+            reader.Close();
+            response.Close();
+
+            DeleteFile();
+
+            var rootPath = $@"E:\Users\Niels\source\repos\WebApplication2\WebApplication2\"; //get the root path
+
+            var fullPath = Path.Combine(rootPath, NombreArchivo);
+
+            var Data = System.IO.File.ReadAllText(fullPath);
+            return Data;
+        }
+
+        private void DeleteFile()
+        {
+            const string site = "ftp://files.000webhost.com//";
+            string NombreArchivo = $"LICEO ROSS.txt";
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(site + @"\" + NombreArchivo);
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+            request.Credentials = new NetworkCredential("niels1709", "niels1709");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+
+        }
+
+        private List<Json> Get(string url)
+        {
+
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+
+                request.Method = "POST";
+                string data = String.Empty;
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    data = reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                }
+
+                #region Prueba_JSON
+                /*data = @"[{""tipoRegistro"": ""E"",""fechaRemision"": ""02072022"",""codigoCentro"": ""00639"",""codigoPlantel"": ""19333469"",""numeroRegistros"": ""2""},{""tipoRegistro"": ""D"",""codigoCentroMinerd"": ""1"",""rne"": ""CBD0406190000"",""municipioResidencia"": ""SANTO DOMINGO ESTE"",""periodoAcademico"": ""P1"",""nivelAcademico"": ""S"",""gradoAcademico"": ""4"",""informacionAcademica"": ""LIT:90,MAT:87,NAT:93,SOC:70"",""tipoSangre"" : ""O+"",""condicionesDiscapacidad"" : ""NINGUNA""},{""tipoRegistro"": ""D"",""codigoCentroMinerd"": ""2"",""rne"": ""HUD0107200000"",""municipioResidencia"": ""SANTO DOMINGO ESTE"",""periodoAcademico"": ""P1"",""nivelAcademico"": ""S"",""gradoAcademico"": ""4"",""informacionAcademica"": ""LIT:98,MAT:87,NAT:77,SOC:88"",""tipoSangre"" : ""O+"",""condicionesDiscapacidad"" : ""NINGUNA""}]";*/
+                #endregion
+                var Datos = JsonConvert.DeserializeObject<List<Json>>(data);
+                return Datos;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         public IActionResult Privacy()
         {
             return View();
---
+++ WebApplication2/Dto/Json.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Dto
+{
+    public class Json
+    {
+        public string TipoRegistro { get; set; }
+        public string FechaRemision { get; set; }
+        public string CodigoCentro { get; set; }
+        public string CodigoPlantel { get; set; }
+
+        public string CodigoCentroMinerd { get; set; }
+
+        public string Rne { get; set; }
+        public string NivelAcademico { get; set; }
+        public string PeriodoAcademico { get; set; }
+        public string GradoAcademico { get; set; }
+        public string InformacionAcademica { get; set; }
+
+        public string MunicipioResidencia { get; set; }
+
+        public string TipodeSangre { get; set; }
+        public string CondicionesDiscapacidad { get; set; }
+
+    }
+}
